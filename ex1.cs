33c69c2cd55406a51101b244c3d79e807290dcfb
Enzzo D' Lucca Blengini e Silva <163877681+DLuCcA13@users.noreply.github.com>
2024-06-25 23:55:41 -0300

    first commit

---
+++ ex1.cs
@@ -0,0 +1,15 @@
+/* Crie um método chamado ImprimirSaudacao que não receba parâmetros e apenas imprima "Olá, bem-vindo ao curso de C#!" na tela. */
+using System;
+
+class Program
+{
+    static void frase()
+    {
+        Console.Write("Olá, bem-vindo ao curso de C#!");
+    }
+  
+    public static void Main(string[] args)
+    {
+        frase();
+    }
+}
